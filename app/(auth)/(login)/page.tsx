8ed20979ae90051cf8ab02be75b2e05ef83867af
Raul <desarrollador1982@gmail.com>
2025-07-23 17:47:11 +0200

    feat: agregar imagen de fondo en la página de inicio de sesión

--- app/(auth)/(login)/page.tsx
+++ app/(auth)/(login)/page.tsx
@@ -11,6 +11,12 @@ export default async function Login() {
 
 	return (
 		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+			<img
+				src="img/bg-login.jpg"
+				className="w-full h-full object-cover absolute top-0 left-0 -z-1"
+				loading="lazy"
+				alt=""
+			/>
 			<div className="w-full max-w-sm">
 				<LoginForm />
 			</div>
