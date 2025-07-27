053e1476d0c87e5aa75a79b486ff504b83c93e9b
Raul <desarrollador1982@gmail.com>
2025-07-27 12:07:39 +0200

    mejora: actualizar el componente DataTable para mostrar el total de datos y optimizar la paginación

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -176,36 +176,34 @@ export function DataTable<TData, TValue>({
 			</div>
 			{/* footer pagination */}
 			<div className="flex items-center justify-end px-2 mt-4">
-				{/* <div className="flex-1 text-sm text-muted-foreground">
-					{table.getFilteredSelectedRowModel().rows.length} of{' '}
-					{table.getFilteredRowModel().rows.length} row(s) selected.
-				</div> */}
+				<div className="flex-1 text-xs text-muted-foreground">
+					{/* {table.getFilteredSelectedRowModel().rows.length} of{' '} */}
+					{table.getFilteredRowModel().rows.length} datos totales
+				</div>
 				<div className="flex items-center space-x-6 lg:space-x-8">
-					<div className="flex items-center space-x-2">
-						<p className="text-sm font-medium">Mostrar</p>
-						<Select
-							value={`${table.getState().pagination.pageSize}`}
-							onValueChange={(value) => {
-								table.setPageSize(Number(value));
-							}}>
-							<SelectTrigger className="h-8 w-[70px]">
-								<SelectValue
-									placeholder={table.getState().pagination.pageSize}
-								/>
-							</SelectTrigger>
-							<SelectContent side="top">
-								{[5, 10, 20, 30, 40, 50]
-									.filter(
-										(pageSize) => !limitShow || pageSize <= (limitShow ?? 10)
-									)
-									.map((pageSize) => (
+					{!limitShow && (
+						<div className="flex items-center space-x-2">
+							<p className="text-sm font-medium">Mostrar</p>
+							<Select
+								value={`${table.getState().pagination.pageSize}`}
+								onValueChange={(value) => {
+									table.setPageSize(Number(value));
+								}}>
+								<SelectTrigger className="h-8 w-[70px]">
+									<SelectValue
+										placeholder={table.getState().pagination.pageSize}
+									/>
+								</SelectTrigger>
+								<SelectContent side="top">
+									{[5, 10, 20, 30, 40, 50].map((pageSize) => (
 										<SelectItem key={pageSize} value={`${pageSize}`}>
 											{pageSize}
 										</SelectItem>
 									))}
-							</SelectContent>
-						</Select>
-					</div>
+								</SelectContent>
+							</Select>
+						</div>
+					)}
 					<div className="flex w-[100px] items-center justify-center text-sm font-medium">
 						Página {table.getState().pagination.pageIndex + 1} de{' '}
 						{table.getPageCount()}
