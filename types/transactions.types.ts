1863fe9bbc8438f75840dc0c21935a7364de7236
Raul <desarrollador1982@gmail.com>
2025-12-27 18:15:52 +0100

    feat: añade los tipos `TransactionWithRelations` y `RecurringTransactionWithRelations` para transacciones con relaciones.

--- types/transactions.types.ts
+++ types/transactions.types.ts
@@ -1,9 +1,18 @@
-import { Prisma } from '@prisma/client';
+import { Prisma } from '@prisma/client'
 
 // Define el tipo extendido con las relaciones
 export type TransactionWithRelations = Prisma.TransactionGetPayload<{
 	include: {
-		wallet: true;
-		category: true;
-	};
-}>;
+		wallet: true
+		category: true
+		recurringTransaction: true
+	}
+}>
+
+export type RecurringTransactionWithRelations =
+	Prisma.RecurringTransactionGetPayload<{
+		include: {
+			wallet: true
+			category: true
+		}
+	}>
